4157dc66b0e347d4d6b680cf92d34810c2477d45
Nurdin <mrsunnysummer@gmail.com>
2024-08-08 16:22:35 +0700

    Chapter 1 format

---
+++ ch_1_format/ch_1_format.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// Format menggunakan printf
+
+	name := "Nurdin"
+	age := 20
+
+	// Membuat print format
+	fmt.Printf("Hello my name is %s and im %d years old\n", name, age)
+}
